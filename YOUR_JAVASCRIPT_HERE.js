c1c12249ea507cb47f151836ee553b1d24138d29
jhhest <janvanhest@outlook.com>
2020-01-11 14:18:07 +0100

    Finished all requirements of Game logic functions

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,11 +1,22 @@
-// Write your JS here
+// Hero Object.
 const hero = {
-    name: "Super hero Jan van Hest",
-    heroic: true,
-    inventory: [],
-    health: 10,
-    weapon: {
-        type: "Dagger",
-        damage: 2
-    }
+  name: "Super hero Jan van Hest",
+  heroic: true,
+  inventory: [],
+  health: 10,
+  weapon: {
+    type: "Dagger",
+    damage: 2
+  }
 };
+
+// Game Logic functions.
+function rest() {
+  return null;
+}
+function pickUpItem() {
+  return null;
+}
+function equipWeapon() {
+  return null;
+}
