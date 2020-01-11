6d9df256c1990c5d0825cd09836098916e097d9c
jhhest <janvanhest@outlook.com>
2020-01-11 16:31:35 +0100

    Section 3: Linked an external stylesheet and made all the pictures the same size

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -13,8 +13,6 @@ const hero = {
   }
 };
 
-
-
 /**
  * Game Logic functions.
  *
@@ -27,59 +25,55 @@ const hero = {
  * rest()
  */
 function rest(person) {
-    if (person.health === 10) {
-        window.alert("Your health is already at the value of 10");
-    }
-    console.log("Rest(): Set healt to 10");
-    person.health = 10;
-    console.log("Rest() -> Check if health is 10:", person.health);
-    return person;
+  if (person.health === 10) {
+    window.alert("Your health is already at the value of 10");
+  }
+  console.log("Rest(): Set healt to 10");
+  person.health = 10;
+  console.log("Rest() -> Check if health is 10:", person.health);
+  return person;
 }
 
 /**
  * pickupItem()
  */
 function pickUpItem(person, weapon) {
-    const weaponToPickup = weapon;
+  const weaponToPickup = weapon;
 
-    person.inventory.push(weaponToPickup);
-    return person;
+  person.inventory.push(weaponToPickup);
+  return person;
 }
 
 /**
  * equipWeapon
  */
 function equipWeapon(person) {
-    const inventoryCount = person.inventory.length;
-    const weapon = person.inventory[0]
-    if( inventoryCount >= 1){
-        person.weapon = weapon;
-        return person;
-    }
+  const inventoryCount = person.inventory.length;
+  const weapon = person.inventory[0];
+  if (inventoryCount >= 1) {
+    person.weapon = weapon;
     return person;
+  }
+  return person;
 }
-// When the bag is clicked it will equip the hero with the first item in the inventory array
-// TODO: And reassigns the `weapon` property of person to the first element of the person.inventory array
-// TODO: If the inventory array of person is empty, the function should do nothing
 
 /**
- * UI and UI events. 
+ * UI and UI events.
  */
 
 // When clicked will reset the `health` property on the hero global variable to 10
 // FIXME: Did not work: document.getElementById("inn").addEventListener("click", rest(hero));
-document.getElementById("inn").addEventListener("click", function() { 
-    rest(hero);
+document.getElementById("inn").addEventListener("click", function() {
+  rest(hero);
 });
-
 
 // When the dagger is clicked it will add a weapon object to the inventory array with `type`: `dagger` and `damage`: 2
 document.getElementById("dagger").addEventListener("click", function() {
-    const weapon = { type: "dagger", damage: 2 };
-    hero.inventory.push(weapon);
-    // FIXME: pickUpItem(hero, weapon); 
+  const weapon = { type: "dagger", damage: 2 };
+  hero.inventory.push(weapon);
+  // FIXME: pickUpItem(hero, weapon);
 });
 
 document.getElementById("bag").addEventListener("click", function() {
-    equipWeapon(hero);
+  equipWeapon(hero);
 });
