55b61269587d46d9f77af0dea8081acbe6ba76d0
jhhest <janvanhest@outlook.com>
2020-01-11 14:14:47 +0100

    Finished all requirements of hero property

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,11 @@
 // Write your JS here
+const hero = {
+    name: "Super hero Jan van Hest",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "Dagger",
+        damage: 2
+    }
+};
