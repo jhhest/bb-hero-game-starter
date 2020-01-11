417e024fce70e708d964139cda992c0c5e2403e9
jhhest <janvanhest@outlook.com>
2020-01-11 15:41:46 +0100

    Finished all the requirements of Picking up items

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,4 +1,7 @@
-// Hero Object.
+/**
+ * Hero Object
+ */
+
 const hero = {
   name: "Super hero Jan van Hest",
   heroic: true,
@@ -10,19 +13,49 @@ const hero = {
   }
 };
 
-// Game Logic functions.
+// When clicked will reset the `health` property on the hero global variable to 10
+document.getElementById("inn").addEventListener("click", function() {
+  hero.health = 10;
+});
 
-// Person is a placeholder for the Hero object.
+// When the dagger is clicked it will add a weapon object to the inventory array with `type`: `dagger` and `damage`: 2
+document.getElementById("dagger").addEventListener("click", function() {
+    const weapon = { type: "dagger", damage: 2 };
+    hero.inventory.push(weapon);
+});
+
+/**
+ * Game Logic functions.
+ *
+ * rest()
+ * pickupItem()
+ * equipWeapon()
+ */
+
+/**
+ * rest()
+ */
 function rest(person) {
-    if(person.health === 10){
-        window.alert("Your health is already at the value of 10");
-    }
+  if (person.health === 10) {
+    window.alert("Your health is already at the value of 10");
+  }
+  console.log("Rest(): Set healt to 10");
   person.health = 10;
+  console.log("Rest() -> Check if health is 10:", person.health);
   return person;
 }
-function pickUpItem() {
+
+/**
+ * pickupItem()
+ */
+function pickUpItem(person, weapon) {
+  person.inventory.push(weapon);
   return null;
 }
+
+/**
+ * equipWeapon
+ */
 function equipWeapon() {
   return null;
 }
