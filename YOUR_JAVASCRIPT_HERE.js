6da72810e0b273e146c208bc9d1419301632933a
jhhest <janvanhest@outlook.com>
2020-01-11 20:26:50 +0100

    Added section 4: displayStats() and displayName()

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -3,32 +3,45 @@
  */
 
 const hero = {
-    name: "Super hero Jan van Hest",
-    heroic: true,
-    inventory: [],
-    health: 10,
-    weapon: {
-        type: "Dagger",
-        damage: 2
-    }
+  name: "Super hero Jan van Hest",
+  heroic: true,
+  inventory: [
+    { type: "sword", damage: 5 },
+    { type: "axe", damage: 10 }
+  ],
+  health: 10,
+  weapon: {
+    type: "Dagger",
+    damage: 2
+  }
 };
 
-const weapon = { type: "dagger", damage: 2 }; // FIXME: Is this okey to do? 
+const weapon = { type: "dagger", damage: 2 }; // FIXME: Is this okey to do?
 
 /**
  * Game Logic functions.
  *
+ * askName()
  * rest()
  * pickupItem()
  * equipWeapon()
  */
 
+function askName(person) {
+    const name = prompt("What is your name? ");
+    person.name = name;
+    return person;
+}
+
+askName(hero);
+
 /**
  * rest()
  */
 function rest(person) {
-    healthValue = person.health
-  if (healthValue === 10) { // FIXME: changed person.health to healtValue to make code more readable. // Error: Error: global leak detected: healthValue
+  const healthValue = person.health;
+  if (healthValue === 10) {
+    // FIXME: changed person.health to healtValue to make code more readable.
     window.alert("Your health is already at the value of 10");
   }
   person.health = 10;
@@ -41,6 +54,7 @@ function rest(person) {
 function pickUpItem(person, weapon) {
   const weaponToPickup = weapon;
   person.inventory.push(weaponToPickup);
+  displayStats();
   return person;
 }
 
@@ -75,3 +89,60 @@ document.getElementById("dagger").addEventListener("click", function() {
 document.getElementById("bag").addEventListener("click", function() {
   equipWeapon(hero);
 });
+
+displayStats();
+/**
+ * displayStats
+ */
+function displayStats() {
+  const statsSection = document.getElementById("stats");
+  statsSection.innerHTML = "";
+  statsSection.appendChild(generateStats());
+}
+
+/**
+ * generateStats()
+ */
+
+function generateStats() {
+  const name = hero.name;
+  const nameElement = document.createElement("li");
+  nameElement.innerHTML = `Name: ${name}`;
+
+  const health = hero.health;
+  const healthElement = document.createElement("li");
+  healthElement.innerHTML = `Health: ${health}HP`;
+
+  const weaponType = hero.weapon.type;
+  const weaponTypeElement = document.createElement("li");
+  weaponTypeElement.innerHTML = `Weapon: ${weaponType}`;
+
+  const weaponDamage = hero.weapon.damage;
+  const weaponDamageElement = document.createElement("li");
+  weaponDamageElement.innerHTML = `Weapon damage: ${weaponDamage}`;
+
+  const heroInventory = hero.inventory.map(item => item.type).join(", ");
+  const inventoryElement = document.createElement("li");
+  inventoryElement.innerHTML = `Inventory: ${heroInventory}`;
+
+  const htmlStatisticsElement = document.createElement("ul");
+  htmlStatisticsElement.appendChild(nameElement);
+  htmlStatisticsElement.appendChild(healthElement);
+  htmlStatisticsElement.appendChild(weaponTypeElement);
+  htmlStatisticsElement.appendChild(weaponDamageElement);
+  htmlStatisticsElement.appendChild(inventoryElement);
+
+  return htmlStatisticsElement;
+}
+
+/*
+TODO:- A player can choose their own name
+- Allow people to change the name of their hero, you can use window.prompt or use an input element. Be careful using a form because using a form might refresh the page.
+TODO:- Make it possible to fight enemies
+- Implement fighting an enemy. (how you do it is up to you!)
+- When an enemy or weapon gets clicked it gets deleted from the page
+
+
+
+
+*/
